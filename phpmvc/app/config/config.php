bbaeed49e1328dc653f7ab6b8dbfcbec35926fc0
realistarrr <75973517+realistarrr@users.noreply.github.com>
2022-10-14 14:27:09 +0700

    Delete phpmvc directory

--- phpmvc/app/config/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-define('BASEURL', 'http://localhost/prakweb_b_203040069/mvc/public');
-
-// DataBase
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'mvc');
