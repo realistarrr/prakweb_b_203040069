d87c4c4219efd66450a8d7e9e46a8faa4c670d05
realistarrr <75973517+realistarrr@users.noreply.github.com>
2022-10-14 21:35:14 +0700

    menyelesaikan tugas #3 phpmvc

---
+++ phpmvc/app/views/home/index.php
@@ -0,0 +1,12 @@
+<div class="container">
+  
+<div class="jumbotron">
+    <h1 class= "display-4"> Hello world</h1>
+    <p class="lead"> Hallo, nama saya <?= $data ['nama']?> </p>
+    <hr class = "my-4">
+    <p> it uses utility. </p>
+    <a class="btn btn-primary btn-lg" href="#" role="button">
+        learn more</a>
+</div>
+</div>
+
