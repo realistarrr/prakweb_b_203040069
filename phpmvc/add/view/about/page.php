89cf8e4dc9999904a70b6f0149a3d97090ccdfae
realistarrr <75973517+realistarrr@users.noreply.github.com>
2022-10-14 07:20:49 +0700

    menyelesaikan tugas #3 phpmvc

---
+++ phpmvc/add/view/about/page.php
@@ -0,0 +1,3 @@
+<div class="container-fluid">
+  <h1>My Page</h1>
+</div>
---
+++ phpmvc/add/view/home/index.php
@@ -0,0 +1,4 @@
+<div class="container-fluid">
+  <h1>Selamat Datang di Home</h1>
+  <p>Halo, Nama Saya <?= $data['nama']; ?></p>
+</div>
